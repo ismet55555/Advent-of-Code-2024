5993bf8af47c32e1d339dafec87be1dc06ce90ec
Ismet Handzic <ismet.handzic@gmail.com>
2024-11-28 22:14:56 -0500

    setup

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Another year, another attempt at this fun thing!
 
 Language: [Rust](https://www.rust-lang.org/)
 
+## Running Code
+
+- Run the code
+
+  - ```sh
+    cd day_x
+    cargo run --release
+    ```
+
+- To actively monitor code issues use `bacon`:
+
+  - Install [`bacon`](https://github.com/Canop/bacon): `cargo install --locked bacon`
+
+  - ```sh
+    bacon --all-features --job clippy
+    ```
--- day_template/src/main.rs
+++ day_template/src/main.rs
@@ -1,16 +1,51 @@
 use std::fs;
 
+//////////////////////////////////////////////////////////////////////////////
+
 const INPUT_FILEPATH: &str = "inputs/sample_part1.txt";
 
-fn main() {
+fn load_input_file() -> Vec<&str> {
     println!("Loading file: {} ...", INPUT_FILEPATH);
     let contents = fs::read_to_string(INPUT_FILEPATH).expect("ERROR: Failed to read input file");
 
     // Divide the lines in file into a vector of strings
-    let input_lines: Vec<&str> = contents.split("\n").collect();
+    let input_lines: Vec<&str> = contents.split("\n").collect()
+    input_lines
 
-    // Print the first line of the input file
-    for line in &input_lines {
-        println!("{}", line);
+    // // Print the first line of the input file
+    // for line in &input_lines {
+    //     println!("{}", line);
+    // }
+}
+
+fn part_1() {
+    todo!("part_1")
+}
+
+fn part_2() {
+    todo!("part_2")
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+fn main() {
+    let part_1_input = load_input_file();
+    let part_1 = part_1();
+
+    let part_2_input = load_input_file();
+    let part_2 = part_2();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        // let expected =
+        // let acutal =
+        // assert_eq!(expected, acutal);
     }
 }
