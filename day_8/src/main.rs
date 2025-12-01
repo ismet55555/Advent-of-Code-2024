6ca31c888f15b18f59c8d6cb939522c1f0c44ca2
Ismet Handzic <ismet.handzic@gmail.com>
2025-11-30 21:24:36 -0500

    wip

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -5,7 +5,7 @@
 #![allow(dead_code)]
 
 use anyhow::Result;
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
@@ -56,29 +56,96 @@ fn look_around_antena(
     antena_symbol: char,
     pos: Position,
     rel_distance: i32,
-) -> Vec<((i32, i32), char)> {
-    let out: Vec<((i32, i32), char)> = vec![((-2, 1), '|')]; // example input, do i need position?
+) -> Vec<(char, char, i32)> {
+    // Return: antena symbol, direction, distance
+    let mut out: Vec<(char, char, i32)> = vec![]; // example input, do i need position?
+
+    let (grid_rows_total, grid_col_total) = get_grid_dimensions(grid);
 
     // Go around "distance" from "pos" in all 8 directions
     // Check match on symbol
     // If symbol matches, store it in "out"
 
-    for search_direction in SEARCH_DIRECTIONS.iter() {}
+    println!("Antena '{}' Position: ({},{})", antena_symbol, pos.0, pos.1);
 
-    // Back at caller, use the rel distance and direction to draw antinodes
+    let row_pos = pos.0 as i32;
+    let col_pos = pos.1 as i32;
+
+    for (search_direction, (row_step, col_step)) in SEARCH_DIRECTIONS.iter() {
+        let search_row_pos = row_pos + *row_step;
+        let search_col_pos = col_pos + *col_step;
+        print!(
+            " - Step: ({},{})  '{}'  -> ({},{})",
+            row_step, col_pos, search_direction, search_row_pos, search_col_pos
+        );
+
+        // If out of bounds, skip
+        if search_row_pos + 1 > grid_rows_total
+            || search_row_pos < 0
+            || search_col_pos + 1 > grid_col_total
+            || search_col_pos < 0
+        {
+            println!(" -> Out of Bounds");
+            continue;
+        }
+
+        // Get symbol at search location
+        let grid_symbol = grid[search_row_pos as usize][search_col_pos as usize];
+        println!(" -> {}", grid_symbol);
+
+        // Skip anything that is not antena
+        if grid_symbol != antena_symbol {
+            continue;
+        }
+        println!("Found antena {} - distance {}", antena_symbol, rel_distance);
+        out.push((antena_symbol, *search_direction, rel_distance));
+    }
 
     out
+}
+
+fn find_distinct_antena_symbols(grid: &Grid) -> HashSet<char> {
+    let mut antena_symbols: HashSet<char> = HashSet::new();
+
+    for row_pos in 0..grid.len() {
+        for col_pos in 0..grid[0].len() {
+            if grid[row_pos][col_pos] != '.' {
+                antena_symbols.insert(grid[row_pos][col_pos]);
+            }
+        }
+    }
+
+    antena_symbols
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
 /// Part 1 Calculations
 fn part_1(input: String) -> i32 {
-    println!("{}", input);
+    // println!("{}", input);
 
     let grid: Grid = input.lines().map(|line| line.chars().collect()).collect();
+    let grid_new = grid.clone();
 
-    // loop around
+    let unique_antena_symbols = find_distinct_antena_symbols(&grid);
+    println!("{:?}", unique_antena_symbols);
+
+    // look_around_antena(&grid, '0', (2_usize, 5_usize), 1);
+
+    for antena_symbol in unique_antena_symbols {
+        println!(
+            "---------------  Antena: {}  ----------------",
+            antena_symbol
+        );
+        for row_pos in 0..grid.len() {
+            for col_pos in 0..grid[0].len() {
+                if grid[row_pos][col_pos] != '.' {
+                    let antinode_findings =
+                        look_around_antena(&grid, antena_symbol, (row_pos, col_pos), 1);
+                }
+            }
+        }
+    }
 
     11
 }
